89c2f55f68c69d87c227699fdf7ca6a9eebdeede
ricardo guerrero <richi_gro@hotmail.com>
2020-08-05 19:34:45 -0500

    add a whole bumch of stuff

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+//Middleware
+app.use(express.static("public"));
+
+
+const PORT = 8081;
+app.listen(PORT, () => {
+  console.log(`app started on port${PORT}`);
+});
